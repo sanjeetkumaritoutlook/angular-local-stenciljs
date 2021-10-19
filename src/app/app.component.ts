97cd73a43feaf7dbbd79f9486be0d8269cb3e406
ChantalSchot <chantalschot@outlook.com>
2021-10-19 20:37:12 +0200

    Add web components to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-cop';
+  innerText: string | undefined;
+
+  @HostListener('buttonClicked', ['$event'])
+  buttonClickHandler(event: any) {
+    console.log(event);
+    this.innerText = `Button clicked was: ${event.detail.id}`;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from 'stenciljs-cop/loader';
+
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +12,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+defineCustomElements();
