ef8a36bf637e638f277179a881778ac5e15251c4
unknown <sanjeetkumarit@outlook.com>
2024-12-14 14:48:10 +0530

    matching default project name to github repo for github pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'angular-cop'`, () => {
+  // it(`should have as title 'angular-local-stenciljs'`, () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('angular-cop');
+  //   expect(app.title).toEqual('angular-local-stenciljs');
   // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-cop app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-local-stenciljs app is running!');
   });
 });
