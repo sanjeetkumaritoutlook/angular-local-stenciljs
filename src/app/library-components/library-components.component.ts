ce13b06836aafb5788be25ce3386c120d25d28ff
unknown <sanjeetkumarit@outlook.com>
2024-12-16 14:25:24 +0530

    added json-schema form component

--- src/app/library-components/library-components.component.ts
+++ src/app/library-components/library-components.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formSchema } from './form-schema'; // Import schema
 
 @Component({
   selector: 'app-library-components',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./library-components.component.scss']
 })
 export class LibraryComponentsComponent implements OnInit {
+  formSchema = formSchema; // Assign the imported schema to a local variable
+//first option used in html file with Angular's json pipe 
+//option 2:  schemaString = JSON.stringify(formSchema); // Convert schema to JSON string
+//and then in html file
+//<json-schema-form [schema]="schemaString"></json-schema-form>
+//
 
   constructor() { }
 
